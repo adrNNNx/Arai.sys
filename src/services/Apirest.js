915166fbf1896530cea6183ca4fb5841d967ac71
adrNNNx <adrianportillo182@gmail.com>
2023-08-11 21:55:17 -0400

    agregamos el dashboard y sus dependencias

--- src/services/Apirest.js
+++
@@ -1,3 +0,0 @@
-export const apiUrl= 'https://jsonplaceholder.typicode.com/users';
-export const apiUrlAuth = 'https://dummyjson.com/auth/login';
-export const apiUrlSoloData= 'http://api.solodata.es/';
